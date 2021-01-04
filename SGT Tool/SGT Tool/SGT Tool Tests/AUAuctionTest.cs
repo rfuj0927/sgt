dee2f7fc5800cfaaf939a158feffa0368ce0e4e0
rfuj0927 <robbiefujiwara@gmail.com>
2021-01-04 11:10:55 +1100

    .

---
+++ SGT Tool/SGT Tool/SGT Tool Tests/AUAuctionTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SGT_Tool.AsxAuctionSimulator;
+using System.Collections.Generic;
+using static SGT_Tool.SingleAsxAuctionSimulator;
+
+namespace SGT_Tool_Tests
+{
+    [TestClass]
+    public class AUAuctionTest
+    {
+        [TestMethod]
+        public void BasicAuctionTestMethod()
+        {
+            List<DisplayOrder> bids = new List<DisplayOrder>();
+            List<DisplayOrder> asks = new List<DisplayOrder>();
+
+            bids.Add(new DisplayOrder(1, 100));
+            bids.Add(new DisplayOrder(0.99, 500));
+
+            asks.Add(new DisplayOrder(0.99, 500));
+            asks.Add(new DisplayOrder(0.99, 500));
+
+            AuctionMatchResult r = AsxAuctionMatchCalculator.Calc(bids, asks);
+            Assert.AreEqual(0.99, r.IEP);
+            Assert.AreEqual(600, r.IEV);
+        }
+    }
+}
---
+++ SGT Tool/SGT Tool/SGT Tool Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SGT Tool Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SGT Tool Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bc693f2a-3265-41f4-8f27-0c33d8715c0b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
